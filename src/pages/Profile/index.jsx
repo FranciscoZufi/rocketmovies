00f6d73b5547b10d0358f0debdb62e53b44b96b7
Francisco <francar76@hotmail.com>
2023-01-23 09:32:32 -0300

    commit

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -3,28 +3,60 @@ import { Link } from 'react-router-dom'
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
+import { useAuth } from '../../hooks/auth'
+import { api } from '../../services/api'
+import { useState } from 'react'
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
 
 
 export function Profile(){
+  const {user, updateProfile} = useAuth()
+
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [passwordOld, setPasswordOld] = useState()
+  const [passwordNew, setPasswordNew] = useState()
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+  const [avatar, setAvatar] = useState(avatarUrl)
+  const [avatarFile, setAvatarFile] = useState(null)
+
+
+  async function handleUpdate(){
+    const updated = {
+      name, email, password: passwordNew, old_password: passwordOld
+    }
+    const userUpdated = Object.assign(user, updated)
+    
+    await updateProfile({user: userUpdated, avatarFile})
+  }
+  function handleChangeAvatar(event){
+    const file = event.target.files[0]
+    setAvatarFile(file)
+
+    const imagePreview = URL.createObjectURL(file)
+    setAvatar(imagePreview)
+  }
+
   return (
     <Container>
       <header>
-        <Link to='/'>
+        <Link to='/home'>
           <FiArrowLeft /> Back 
           </Link>
       </header>
       <Form>
-        <Avatar> <img src="https://github.com/franciscozufi.png" alt="Foto do usuário" />
+        <Avatar> <img src={avatar} alt="Foto do usuário" />
         <label htmlFor="avatar">
           <FiCamera />
-          <input id="avatar" type="file"/>
+          <input id="avatar" type="file" onChange={handleChangeAvatar}/>
         </label>
         </Avatar>
-        <Input placeholder="Name" type="text" icon={FiUser}/>
-        <Input placeholder="E-mail" type="text" icon={FiMail}/>
-        <Input placeholder="Current password" type="Password" icon={FiLock}/>
-        <Input placeholder="New password" type="Password" icon={FiLock}/>
-        <Button title="Save" />
+        <Input placeholder="Name" type="text" icon={FiUser} value={name} onChange={e => setName(e.target.value)}/>
+        <Input placeholder="E-mail" type="text" icon={FiMail} value={email} onChange={e => setEmail(e.target.value)}/>
+        <Input placeholder="Current password" type="Password" icon={FiLock} onChange={e => setPasswordOld(e.target.value)}/>
+        <Input placeholder="New password" type="Password" icon={FiLock} onChange={e => setPasswordNew(e.target.value)}/>
+        <Button title="Save" onClick={handleUpdate}/>
       </Form>
     </Container>
   )
--- src/pages/SingUP/index.jsx
+++ src/pages/SingUP/index.jsx
@@ -1,11 +1,34 @@
+import {useState} from 'react'
 import { Container, Form, Background } from './styles'
 import { Input } from '../../components/Input'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '../../components/Button'
-import { FiMail, FiLock, FiUser, FiArrowLeft} from 'react-icons/fi'
+import { FiMail, FiLock, FiUser} from 'react-icons/fi'
+import { api } from '../../services/api'
 
 
 export function SingUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  function handleSignUp(){
+    if(!name || !email || !password) {
+      return alert('Fill in all fields!')
+    }
+    api.post('/users', {name, email, password}).then(() => {
+      alert('Successfully registered user')
+      navigate('/')
+    }).catch( error => {
+      if(error.response){
+        alert(error.response.data.message)
+      } else {
+        alert('Could not register')
+      }
+    })
+  }
 return (
   <Container>
     <Background/>
@@ -14,10 +37,10 @@ return (
       <p>Application to track everything you watch.</p>
 
       <h2>Create account</h2>
-      <Input placeholder="Name" type="text" icon={FiUser}/>
-      <Input placeholder="E-mail" type="text" icon={FiMail}/>
-      <Input placeholder="Password" type="password" icon={FiLock}/>
-      <Button title="Register"/>
+      <Input placeholder="Name" type="text" icon={FiUser} onChange={e => setName(e.target.value)}/>
+      <Input placeholder="E-mail" type="text" icon={FiMail} onChange={e => setEmail(e.target.value)}/>
+      <Input placeholder="Password" type="password" icon={FiLock} onChange={e => setPassword(e.target.value)}/>
+      <Button title="Register" onClick={handleSignUp}/>
       <Link to="/"> <FiArrowLeft/> Back to LogIn</Link>
     </Form>
   </Container>
