dd42c8a2d5c0b99f6cf07f64476a94c1e50907ba
Francisco <francar76@hotmail.com>
2023-01-23 16:04:02 -0300

    commit

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,21 +1,38 @@
-
+import { useAuth } from '../../hooks/auth'
+import {  FiSearch } from 'react-icons/fi'
+import { api } from '../../services/api'
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+import { useNavigate } from 'react-router-dom';
 import { Container, Profile, Logout, Span } from "./styles";
+import { Input } from '../Input';
+
 
 
 export function Header(){
+  const { signOut, user } = useAuth() 
+  const navigate = useNavigate()
+  function handleSignOut(){
+    navigate('/')
+    signOut()
+}
+const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
   return (
     <Container>
       <Span>RocketMovies</Span>
-      <input type="text" placeholder="Search by title" />
+        <Input 
+        type="text" 
+        placeholder="Search by title" 
+        icon={FiSearch}
+        /> 
         <div>
       <Profile to='/profile'>
-          <strong>Francisco Zufi</strong>
+          <strong>{user.name}</strong>
       </Profile>
-          <Logout to='/'>
+          <Logout onClick={handleSignOut}>
              Leave
           </Logout>
         </div>
-        <img src="https://github.com/franciscozufi.png" alt="Foto do usuário" />
+        <img src={avatarUrl} alt={user.name} />
     
     </Container>
   )
